a7cd33a5517c4c5d8ab7782282c5070e67a0bdf1
supercodernoshadow <dadebonojo@icloud.com>
2023-10-30 14:36:32 -0600

    add calendar to listings struct

--- contracts/ResToken.sol
+++ contracts/ResToken.sol
@@ -13,30 +13,57 @@ contract ResToken is ERC1155, Ownable {
 
     struct Listing {
         string name;
+        uint256 id;
+        uint256 nightlyRate;
+        mapping(uint256 => bool) calendar;
     }
-
-    // Mapping from token ID to the listing it belongs to
-    //mapping(uint256 => uint256) public tokenIdToListingId;
 
     mapping(uint256 => Listing) public listings;
+    event Mint(uint256 amount, address minter);
+    event newListing(uint256 id);
 
-    constructor() ERC1155("https://ipfs.io/ipfs/bafybeihjjkwdrxxjnuwevlqtqmh3iegcadc32sio4wmo7bv2gbf34qs34a/{id}.json") Ownable(){}
+    constructor() ERC1155("https://ipfs.io/ipfs/bafybeihjjkwdrxxjnuwevlqtqmh3iegcadc32sio4wmo7bv2gbf34qs34a/{id}.json") Ownable(){
+        _mint(address(this), tokenId.current(), 1e18, "");
+    }
     
     // Create a new listing with a specified name 
-    function createListing(string memory name) external {
+    function createListing(string memory name, uint256 rate) external {
         listingCount.increment();
-        listings[listingCount.current()] = Listing(name);
+        uint256 currentId = listingCount.current();
+
+        listings[currentId].name = name;
+        listings[currentId].id = currentId;
+        listings[currentId].nightlyRate = rate;
+        //listings[listingCount.current()] = Listing(name, listingCount.current(), rate);
+        emit newListing(listingCount.current());
     }
 
+    // Check if nights are open
+    function checkCalendar(uint256 listingId, uint256 startDate, uint256 endDate) internal view returns (bool){
+        bool available = true;
+        for(uint256 i = startDate; i < endDate; i++){
+            if(listings[listingId].calendar[i] == true){
+                available = false;
+            }
+        }
+        return available;
+    }
     // Reserve a listing and create a new reservation token
-    function reserve(uint256 listingId, uint256 startDate, uint256 endDate) external {
+    function reserve(uint256 listingId, uint256 startDate, uint256 endDate) external payable{
+        require(msg.value >= (endDate - startDate) * listings[listingId].nightlyRate, "Broke boi");
         require(startDate < endDate, "Invalid date range");
-        
+        require(checkCalendar(listingId, startDate, endDate) == true);
         tokenId.increment();
         uint256 newTokenId = tokenId.current();
 
+        // Update calendar
+        for(uint256 i = startDate; i < endDate; i++){
+            listings[listingId].calendar[i] = true;
+        }
         _mint(msg.sender, newTokenId, 1, "");
-        //listings[newTokenId] = _listing;
+        emit Mint(1, msg.sender);
+
+
     }
 
     // Transfer a reservation token to another address
@@ -49,7 +76,7 @@ contract ResToken is ERC1155, Ownable {
 
     // Get the details of a listing
     function getListing(uint256 listingId) external view returns (string memory) {
-        //require(listingId < listings.length, "Invalid listing ID");
-        return (listings[listingId].name);
+        require(listingId <= listingCount.current(), "Invalid listing ID");
+        return (listings[listingId]).name;
     }
 }
--- test/ResToken.js
+++ test/ResToken.js
@@ -24,34 +24,51 @@ describe('ResToken', () => {
 
   describe('Success', () => {
 
-    it("creates a listing", async () => {
-      transaction = await token.connect(deployer).createListing("Luxury Villa")
-      result = await transaction.wait()
-      const listing = await token.getListing(1);
-      expect(listing).to.equal("Luxury Villa");
-
-    })
-    it("creates a reservation", async () => {
-      transaction = await token.connect(guest).reserve(1, 3,7)
-      result = await transaction.wait()
-      const guestBalance = await token.balanceOf(guest.address, 1)
-      expect(guestBalance).to.equal(1)
-    })
-    it("transfers a reservation", async () => {
-      let guestBalance
-      transaction = await token.connect(guest).reserve(1, 3,7)
-      result = await transaction.wait()
-      guestBalance = await token.balanceOf(guest.address, 1)
-      expect(guestBalance).to.equal(1)
-      transaction = await token.connect(guest).transferRes(receiver.address, 1)
+    beforeEach(async () => {
+      transaction = await token.connect(deployer).createListing("Luxury Villa", 1)
       result = await transaction.wait()
 
-      const receiverBalance = await token.balanceOf(receiver.address, 1)
-      guestBalance = await token.balanceOf(guest.address, 1)
-      expect(receiverBalance).to.equal(1)
-      expect(guestBalance).to.equal(0)
+    })
+    describe('Listings', () => {
+      it("creates a listing", async () => {
+
+        const listing = await token.getListing(1)
+        expect(listing).to.equal("Luxury Villa")
+
+      })
+      it("emits a create listing event", async () => {
+         expect(transaction).to.emit(token, 'newListing')
+            .withArgs(1)  
+
+      })
     })
 
+    describe('Reservations', () => {
+      beforeEach(async () => {
+      transaction = await token.connect(guest).reserve(1, 3,7, { value: ether(4)})
+      result = await transaction.wait()
+
+      })
+
+      it("creates a reservation", async () => {
+        const guestBalance = await token.balanceOf(guest.address, 1)
+        expect(guestBalance).to.equal(1)
+      })
+      it("transfers a reservation", async () => {
+        transaction = await token.connect(guest).transferRes(receiver.address, 1)
+        result = await transaction.wait()
+
+        const receiverBalance = await token.balanceOf(receiver.address, 1)
+        guestBalance = await token.balanceOf(guest.address, 1)
+        expect(receiverBalance).to.equal(1)
+        expect(guestBalance).to.equal(0)
+      })
+      it("enforces calendar", async () => {
+        expect(token.connect(guest).reserve(1, 3,7, { value: ether(5)})).to.be.reverted
+     
+      })
+
+    })
 
   })
 
