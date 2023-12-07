da3c19377515fb1374912e40f8eec23aaee94c54
supercodernoshadow <dadebonojo@icloud.com>
2023-12-07 10:13:28 -0700

    Update mint handler

--- src/components/Mint.js
+++ src/components/Mint.js
@@ -10,9 +10,10 @@ import { ethers } from 'ethers'
 
 const Mint = ( { id } ) => {
 
-  	const provider = useSelector(state => state.provider)
-  	const token = useSelector(state => state.contracts)
-  	const rates = useSelector(state => state.rates)
+  	const provider = useSelector(state => state.provider.connection)
+  	const token = useSelector(state => state.tokens.contracts)
+  	console.log(token)
+  	const rates = useSelector(state => state.tokens.rates)
 
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
@@ -40,9 +41,10 @@ const Mint = ( { id } ) => {
 		  	{ value: ethers.utils.parseUnits((rates[1] * (endNight - startNight)).toString(), 'ether') } 
 		  	)
 		  await transaction.wait()
-		} catch {
-		  window.alert('User rejected or transaction reverted')
+		} catch(e) {
+		  console.error('User rejected or transaction reverted', e)
 		}
+		setIsWaiting(false)
 
 	}
 	return(
@@ -53,13 +55,14 @@ const Mint = ( { id } ) => {
 		  ) : (
 		  <>
 			<Form.Group className="mb-3">
-                <Form.Label>Start Date</Form.Label>
-                <Form.Control type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Form.Label>End Date</Form.Label>
-				<Form.Control type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
-            </Form.Group>
+			    <Form.Label htmlFor="startDate">Start Date</Form.Label>
+			    <Form.Control type="date" id="startDate" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
+			</Form.Group>
+			<Form.Group className="mb-3">
+			    <Form.Label htmlFor="endDate">End Date</Form.Label>
+			    <Form.Control type="date" id="endDate" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
+			</Form.Group>
+
 
 			<Form.Group>
 				<Button variant="primary" type="submit" style={{ width: '100%'}}>
